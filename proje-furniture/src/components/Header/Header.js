96333ca95b5ca30850a3962716e137124d5d9d60
galip <galipts1@gmail.com>
2023-01-30 20:50:05 +0300

    30.01.2023

--- proje-furniture/src/components/Header/Header.js
+++ proje-furniture/src/components/Header/Header.js
@@ -1,28 +1,87 @@
 import React from "react";
 import { Container, NavLink, Row } from "react-bootstrap";
+import {
+  AiOutlineHeart,
+  AiOutlineMenu,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import userIcon from "../../assets/images/user-icon.png";
 import "./header.scss";
 
 import logo from "../../assets/images/eco-logo.png";
+import { motion } from "framer-motion";
+
+// const nav__links = [
+//   {
+//     path: "home",
+//     display: "Home",
+//   },
+//   {
+//     path: "shop",
+//     display: "Shop",
+//   },
+//   {
+//     path: "cart",
+//     display: "Cart",
+//   },
+// ];
+
 const Header = () => {
   return (
     <header className="header">
-      <Container fluid>
+      <Container>
         <Row>
           <div className="nav__wrapper">
             <div className="logo">
               <img src={logo} alt="logo" />
-            </div>
-            <div>
-              <h1>Our Multimart</h1>
-              <p>Since 2000</p>
-            </div>
 
+              <div>
+                <h1>Pultimart</h1>
+                {/* <p>Since 2000</p>  */}
+              </div>
+            </div>
             <div className="navigation">
               <ul className="menu">
+                {/* {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "nav__active" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))} */}
                 <li className="nav__item">
-                  <NavLink>Home</NavLink>
+                  <NavLink to="home">Home</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="shop">Shop</NavLink>
+                </li>
+                <li className="nav__item">
+                  <NavLink to="cart">Cart</NavLink>
                 </li>
               </ul>
+            </div>
+            <div className="nav__icons">
+              <span className="fav__icon">
+                <AiOutlineHeart />
+                <span className="badge">1</span>
+              </span>
+              <span className="cart__icon">
+                <AiOutlineShopping />
+                <span className="badge">1</span>
+              </span>
+              <span>
+                <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
+              </span>
+            </div>
+            <div className="mobile__menu">
+              <span>
+                <AiOutlineMenu />
+              </span>
             </div>
           </div>
         </Row>
---
+++ proje-furniture/src/services/Services.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./services.scss";
+import { BsTruck } from "react-icons/bs";
+import serviceData from "../assets/data/serviceData";
+import { motion } from "framer-motion";
+
+const Services = () => {
+  return (
+    <section className="services">
+      <Container>
+        <Row>
+          {serviceData.map((item, index) => (
+            <Col lg="3" md="4" key={index}>
+              <motion.div
+                whileHover={{ scale: 1.1 }}
+                className="service__item"
+                style={{ background: `${item.bg}` }}
+              >
+                <span>
+                  <i className={item.icon}></i>
+                </span>
+                <div>
+                  <h3>{item.title} </h3>
+                  <p>{item.subtitle} </p>
+                </div>
+              </motion.div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default Services;

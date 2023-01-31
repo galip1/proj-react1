a1562e67e7812e41390123e04e4fed845e7ee447
galip <galipts1@gmail.com>
2023-01-31 18:39:51 +0300

    n

--- proje-furniture/src/pages/Home.js
+++ proje-furniture/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import Helmet from "../components/Helmet/Helmet";
 import heroImg from "../assets/images/hero-img.png";
@@ -7,9 +7,28 @@ import "../styles/home.scss";
 import { Link } from "react-router-dom";
 import Services from "../services/Services";
 import ProductList from "../components/UI/ProductList";
+import products from "../assets/data/products";
+
+import counterImg from "../assets/images/counter-timer-img.png";
+import Clock from "../components/UI/Clock";
 
 const Home = () => {
+  const [trendingProducts, setTrendingProducts] = useState([]);
+  const [bestSalesProducts, setBestSalesProducts] = useState([]);
   const year = new Date().getFullYear();
+
+  useEffect(() => {
+    const filteredTrendingProducts = products.filter(
+      (item) => item.category === "chair"
+    );
+    const filteredBestSalesProducts = products.filter(
+      (item) => item.category === "sofa"
+    );
+
+    setTrendingProducts(filteredTrendingProducts);
+    setBestSalesProducts(filteredBestSalesProducts);
+  }, []);
+
   return (
     <Helmet title={"Home"}>
       <section className="hero__section">
@@ -42,10 +61,43 @@ const Home = () => {
       <section className="trending__products">
         <Container>
           <Row>
-            <Col lg="12" className="text__contect">
+            <Col lg="12" className="text-center">
               <h2 className="section__title">Trending Products</h2>
             </Col>
-            <ProductList />
+            <ProductList data={trendingProducts} />
+          </Row>
+        </Container>
+      </section>
+      <section className="best__sales">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h2 className="section__title">Best Sales</h2>
+            </Col>
+            <ProductList data={bestSalesProducts} />
+          </Row>
+        </Container>
+      </section>
+
+      <section className="timer__count">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6 mb-2">Limited Offers</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+              </div>
+              <Clock />
+              <motion.button
+                whileTap={{ scale: 1.2 }}
+                className="btn buy__btn store__btn"
+              >
+                <Link to="/shop">Visit Store</Link>
+              </motion.button>
+            </Col>
+            <Col lg="6" md="6" className="text-end">
+              <img src={counterImg} alt="" />
+            </Col>
           </Row>
         </Container>
       </section>
